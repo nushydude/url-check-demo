274662e8e0a62c7c59dca059d738759948fb1ed1
Anusha Dharmasena <cdharmas@officeworks.com.au>
2022-11-04 13:11:23 +1100

    Initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,33 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import validator from 'validator'
+import styled from 'styled-components'
 
-function App() {
+const PageContainer = styled.div`
+  width: 100vw;
+  height: 100vh;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+  font-family: Verdana, Geneva, Tahoma, sans-serif;
+`;
+
+const Input = styled.input<{ error: boolean }>`
+  padding: 10px;
+  border: 2px solid ${({ error }) => error ? 'red' : 'black'};
+  width: 400px;
+  outline: none;
+`;
+
+
+const App: React.FC = () => {
+  const [value, setValue] = useState('')
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <PageContainer>
+      <p>Enter a URL to check:</p>
+      <Input value={value} onChange={(e) => setValue(e.target.value)} error={!validator.isURL(value)} />
+    </PageContainer>
   );
 }
 
